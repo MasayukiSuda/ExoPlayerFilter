39db3ded1bcc3ca5fb2ab62e6a92e0f7b34ba861
Masayuki Suda <topman31daasuu@gmail.com>
2017-05-24 11:09:09 +0900

    add portlate stream url sample

--- sample/src/main/java/com/daasuu/exoplayerfilter/Constant.java
+++ sample/src/main/java/com/daasuu/exoplayerfilter/Constant.java
@@ -9,4 +9,7 @@ public class Constant {
     public static final String STREAM_URL_MP4_VOD_LONG = "http://r8---sn-oguesn7y.c.youtube.com/videoplayback?id=604ed5ce52eda7ee&itag=22&source=youtube&sparams=expire,id,ip,ipbits,mm,mn,ms,mv,nh,pl,source&ip=202.32.147.10&ipbits=0&expire=19000000000&signature=2742A45C170667515836A9FDD75659086F71D917.3D65834800082AC81319F720B62A91E56BD8B4C3&key=cms1&cms_redirect=yes&mm=31&mn=sn-oguesn7y&ms=au&mt=1456997821&mv=m&nh=IgpwcjAyLm5ydDEwKgkxMjcuMC4wLjE&pl=23";
 
     public static final String STREAM_URL_MP4_VOD_SHORT = "http://demos.webmproject.org/exoplayer/glass.mp4";
+
+    public static final String STREAM_URL_PORTLATE = "https://ccs3.akamaized.net/cchanclips/a6164c61eddb455190330e05c6c91ca6/clip.mp4";
+
 }
