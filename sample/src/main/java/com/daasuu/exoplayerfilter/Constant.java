88b918fbafcb091891357027721f9482e293c328
MasayukiSuda <topman31daasuu@gmail.com>
2018-01-27 10:17:53 +0900

    small fix and update EXOPlayer

--- sample/src/main/java/com/daasuu/exoplayerfilter/Constant.java
+++ sample/src/main/java/com/daasuu/exoplayerfilter/Constant.java
@@ -6,7 +6,7 @@ package com.daasuu.exoplayerfilter;
 
 public class Constant {
 
-    public static final String STREAM_URL_MP4_VOD_LONG = "http://r8---sn-oguesn7y.c.youtube.com/videoplayback?id=604ed5ce52eda7ee&itag=22&source=youtube&sparams=expire,id,ip,ipbits,mm,mn,ms,mv,nh,pl,source&ip=202.32.147.10&ipbits=0&expire=19000000000&signature=2742A45C170667515836A9FDD75659086F71D917.3D65834800082AC81319F720B62A91E56BD8B4C3&key=cms1&cms_redirect=yes&mm=31&mn=sn-oguesn7y&ms=au&mt=1456997821&mv=m&nh=IgpwcjAyLm5ydDEwKgkxMjcuMC4wLjE&pl=23";
+    public static final String STREAM_URL_MP4_VOD_LONG = "http://www.html5videoplayer.net/videos/toystory.mp4";
 
     public static final String STREAM_URL_MP4_VOD_SHORT = "http://demos.webmproject.org/exoplayer/glass.mp4";
 
